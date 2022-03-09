700a7dc0866d8e381c04c47e02e78b221af51270
admin <admin@CHETUIWK1625>
2022-03-09 17:07:20 +0530

    Add project files.

---
+++ MVC_project_dropdown/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_project_dropdown.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_project_dropdown.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public ApplicationDbContext dbContext { get; }
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext application)
+        {
+            _logger = logger;
+            dbContext = application;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetCountries()
+        {
+            var countries = dbContext.Countries.ToList();
+            return Json(countries);
+        }
+        public JsonResult GetStates(int id)
+        {
+            var states = dbContext.States.Where(e => e.Country.Id == id).ToList();
+            return Json(states);
+        }
+        public JsonResult GetCities(int id)
+        {
+            var cities = dbContext.Cities.Where(e => e.State.Id == id).ToList();
+            return Json(cities);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_project_dropdown/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_project_dropdown.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string StateName { get; set; }
+        public Country Country { get; set; }
+    }
+}
